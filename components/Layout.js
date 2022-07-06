77eb17a73bd49baa49b019dc672d1f9fe9a2ac52
michael lim <michael.limnh@gmail.com>
2022-07-06 23:44:48 +0800

    Add header

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
+import Header from '../pages/header';
+
 const Layout = ({ children }) => {
     return (
         <div>
+            <Header />
             <main>
                 { children }
             </main>
         </div>
     )
 }
+
+export default Layout;
