19808a431162d0e736bca0936894e57ed707879c
michael lim <michael.limnh@gmail.com>
2022-07-11 21:55:50 +0800

    mobile first approach for header

--- pages/header.js
+++ pages/header.js
@@ -1,7 +1,13 @@
 export default function Header() {
     return (
-        <div className="">
-            synapse
+        <div className="sticky top-0 z-50 w-100 flex flex-col lg:flex-row lg:justify-between align-middle bg-gray-100 py-5 px-6 shadow-lg">
+            <div className="text-3xl lg:text-4xl text-bold mx-auto pb-5 lg:pb-0">SYNAPSE+</div>
+            <div className="flex inline-block align-middle text-teal-800 text-md mx-auto">
+                <a className="pr-5">About</a>
+                <a className="pr-5">Mission</a>
+                <a className="pr-5">Vision</a>
+                <a className="pr-5">Who are we</a>
+            </div>
         </div>
     )
 };
